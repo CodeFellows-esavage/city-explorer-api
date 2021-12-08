bc5ea569638bae3bab0ceb5264e7cc75a8c78eb9
erik savage <erik.k.savage@gmail.com>
2021-12-07 16:20:03 -0800

    adds basic server function

---
+++ server.js
@@ -0,0 +1,13 @@
+'use strict';
+
+require('dotenv').config();
+const express = require('express');
+const cors = require ('cors');
+
+const app = express();
+
+app.use(cors());
+
+const PORT = process.env.PORT;
+
+app.listen(PORT, () => console.log('server is listening on port', PORT));
