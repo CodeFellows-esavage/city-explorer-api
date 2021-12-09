7210103ecfa4e6c12fee38031d2d3913a183883d
erik savage <erik.k.savage@gmail.com>
2021-12-08 16:11:19 -0800

    refactors server to utilize live weather api

--- server.js
+++ server.js
@@ -3,7 +3,7 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
-const weather = require('./data/weather.json');
+const axios = require('axios');
 
 const app = express();
 
@@ -18,29 +18,30 @@ app.get('/weather', handleGetWeather);
 
 class Forcast {
     constructor(obj){
-        this.description = obj.weather.description;
+        this.description = `Low of ${obj.low_temp}, high of ${obj.max_temp} with ${obj.weather.description}`;
         this.date = obj.datetime;
     }
 }
 
 //forEach weather.data[{}] find .weather.description && .datetime
 
-function handleGetWeather(request, response) {
-    console.log(request.query.query);
-    let locationObj = weather.find(obj => obj.city_name === request.query.query);
-    if (locationObj) {
-    //locationObj.data.foreach() clean up
-        let locWeatherObjArr = locationObj.data.map(dataObj => new Forcast(dataObj));
-        console.log(locWeatherObjArr);
-        response.send(locWeatherObjArr);
-    } else {
-        //send some sort of error
-        console.log('no locations found with that name');
-        return response.status(204).send({
-            message: 'no locations found with that name'
+async function handleGetWeather(request, response) {
+    const url = `http://api.weatherbit.io/v2.0/forecast/daily?lat=${request.query.lat}&lon=${request.query.lon}&key=${process.env.WEATHER_API_KEY}&units=I&days=12`;
+    console.log(request.query);
+    console.log(url);
+    try{
+        let result = await axios.get(url);
+        console.log(result.data.data);
+        let weatherObjArr = result.data.data.map(dataObj => new Forcast(dataObj));
+        console.log(weatherObjArr);
+        response.send(weatherObjArr);
+    } catch (error){
+        console.log('No weather data found for location!');
+        response.status(204).send({
+            message: 'No weather data found for location!'
         });
     }
-
 }
+
 
 
