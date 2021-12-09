0917fd6eed6096a0f90b3803e3b0efc7e1b8013b
erik savage <erik.k.savage@gmail.com>
2021-12-08 17:32:18 -0800

    updates backend to query moviedb

--- server.js
+++ server.js
@@ -15,6 +15,7 @@ app.listen(PORT, () => console.log('server is listening on port', PORT));
 
 //routes
 app.get('/weather', handleGetWeather);
+app.get('/movie', handleGetMovie);
 
 class Forcast {
     constructor(obj){
@@ -23,22 +24,46 @@ class Forcast {
     }
 }
 
+class Movie {
+    constructor(obj){
+        this.title = obj.title;
+        this.overview = obj.overview;
+        this.average_votes = obj.vote_average;
+        this.total_votes = obj.vote_count;
+        this.image_url = obj.poster_path;
+        this.popularity = obj.popularity;
+        this.released_on = obj.release_date;
+    }
+}
+
 //forEach weather.data[{}] find .weather.description && .datetime
 
 async function handleGetWeather(request, response) {
     const url = `http://api.weatherbit.io/v2.0/forecast/daily?lat=${request.query.lat}&lon=${request.query.lon}&key=${process.env.WEATHER_API_KEY}&units=I&days=12`;
-    console.log(request.query);
-    console.log(url);
+
     try{
-        let result = await axios.get(url);
-        console.log(result.data.data);
-        let weatherObjArr = result.data.data.map(dataObj => new Forcast(dataObj));
-        console.log(weatherObjArr);
+        const result = await axios.get(url);
+        const weatherObjArr = result.data.data.map(dataObj => new Forcast(dataObj));
         response.send(weatherObjArr);
-    } catch (error){
-        console.log('No weather data found for location!');
+    } catch (error) {
         response.status(204).send({
             message: 'No weather data found for location!'
+        });
+    }
+}
+
+async function handleGetMovie(request, response) {
+    //Jack+Reacher format for multiple strings
+    const url = `https://api.themoviedb.org/3/search/movie?api_key=${process.env.MOVIE_API_KEY}&query=${request.query.loc}`;
+    console.log(request.query.loc);
+    console.log(url);
+    try{
+        const result = await axios.get(url);
+        const movieObjArr = result.data.results.map(movieObj => new Movie(movieObj));
+        response.send(movieObjArr);
+    } catch (error) {
+        response.status(204).send({
+            message: 'No movies about this location!'
         });
     }
 }
