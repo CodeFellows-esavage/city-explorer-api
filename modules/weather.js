6caa20a9acc501489e920cbdeda509368b6ddda9
erik savage <erik.k.savage@gmail.com>
2021-12-11 14:59:53 -0800

    stores retrieved data in cashe

--- modules/weather.js
+++ modules/weather.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const axios = require('axios');
+const cache = require('./cache'); //needs the ./ but not the .js
 
 class Forcast {
     constructor(obj){
@@ -8,18 +9,32 @@ class Forcast {
         this.date = obj.datetime;
     }
 }
-
+//3600000 milliseconds in an hour
 async function handleGetWeather(request, response) {
-    const url = `http://api.weatherbit.io/v2.0/forecast/daily?lat=${request.query.lat}&lon=${request.query.lon}&key=${process.env.WEATHER_API_KEY}&units=I&days=12`;
+    const lat = request.query.lat;
+    const lon = request.query.lon;
+    const url = `http://api.weatherbit.io/v2.0/forecast/daily?lat=${lat}&lon=${lon}&key=${process.env.WEATHER_API_KEY}&units=I&days=12`;
+    const key = `weather-${lat}|${lon}`;
 
-    try{
-        const result = await axios.get(url);
-        const weatherObjArr = result.data.data.map(dataObj => new Forcast(dataObj));
-        response.send(weatherObjArr);
-    } catch (error) {
-        response.status(204).send({
-            message: 'No weather data found for location!'
-        });
+    //check if there is anything in the cache
+    if (cache[key] && (cache[key].timestamp >= Date.now() - 3600000)){
+        console.log('cache hit');
+        response.status(200).send(cache[key].data);
+    } else {
+        console.log('cache.miss');
+        cache[key] = {};
+        cache[key].timestamp = Date.now();
+
+        try{
+            const result = await axios.get(url);
+            const weatherObjArr = result.data.data.map(dataObj => new Forcast(dataObj));
+            cache[key].data = weatherObjArr;
+            response.status(200).send(cache[key].data);
+        } catch (error) {
+            response.status(204).send({
+                message: 'No weather data found for location!'
+            });
+        }
     }
 }
 
